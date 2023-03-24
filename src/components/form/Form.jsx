cb1a4d736727a570a9715ca43906a925c3545481
Bruno Silva <brunosilva@viggosistemas.com.br>
2023-03-24 01:26:37 -0300

    feat: usando useCallback pra melhorar usabilidade
    Adicionei um useCallback pra add tarefa ao pressionar enter no teclado e validar se o campo está
    vazio antes de add, mas ainda falta resolver um warning no todoCreate pedido para usar um
    useCallback também.

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,24 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { TextField, Button, Paper } from '@mui/material'
 
 export default function Form({ handlerAddTodo }) {
-  const [text, setText] = useState(null);
+  const [text, setText] = useState('');
   const [id, setId] = useState(0);
 
   const todoCreate = (text) => {
+    if (text.trim().length === 0) return;
     const todoObj = {text: text, id: id};
     setId(id + 1);
     handlerAddTodo(todoObj);
+    setText('');
   };
+
+  const handleInputKeyDown = useCallback((evt) => {
+    if (evt.key === 'Enter') {
+      if (evt.target.value.trim().length === 0 ) return;
+      todoCreate(evt.target.value);
+      evt.target.value = '';
+      setText(evt.target.value);
+
+    }
+  }, [todoCreate]);
 
   return (
     <Paper style={{padding: "1em"}}>
       <div style={{display: "flex", justifyContent: "center"}}>
         <TextField
           onChange={(evt) => setText(evt.target.value)}
+          onKeyDown={handleInputKeyDown}
           id="outlined-basic"
           label="Tarefa"
           variant="outlined"
+          value={text}
           fullWidth
         />
         <Button
